a510fbff2cc2bda2a569ab541f67d587a7138516
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-05-17 23:54:36 +0300

    enable migrations and add all models in DbContext model

--- HRAgencySystem/HRAgencySystem.Data/HRAgancyDbContext.cs
+++ HRAgencySystem/HRAgencySystem.Data/HRAgancyDbContext.cs
@@ -1,18 +1,33 @@
-using HRAgencySystem.Models;
-using Microsoft.AspNet.Identity.EntityFramework;
+using HRAgencySystem.Data.Migrations;
 
 namespace HRAgencySystem.Data
 {
-    using System;
     using System.Data.Entity;
-    using System.Linq;
+
+    using Microsoft.AspNet.Identity.EntityFramework;
+    
+    using Models;
 
     public class HRAgancyDbContext : IdentityDbContext<User>
     {
         public HRAgancyDbContext()
             : base("HRAgancy", throwIfV1Schema: false)
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<HRAgancyDbContext, Configuration>());
         }
+
+        public virtual IDbSet<Hall> Halls { get; set; }
+
+        public virtual IDbSet<HallStatus> HallStatuses { get; set; }
+
+        public virtual IDbSet<Item> Items { get; set; }
+
+        public virtual IDbSet<Office> Offices { get; set; }
+
+        public virtual IDbSet<Reservation> Reservations { get; set; }
+
+        public virtual IDbSet<UserRole> UserRoles { get; set; }
+
 
         public static HRAgancyDbContext Create()
         {
