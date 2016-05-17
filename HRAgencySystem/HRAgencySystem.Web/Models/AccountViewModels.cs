f13c7f82cf0b2f972473c63f3b1e2a6c22ed0fcd
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-05-18 01:07:38 +0300

    add to register form and username

--- HRAgencySystem/HRAgencySystem.Web/Models/AccountViewModels.cs
+++ HRAgencySystem/HRAgencySystem.Web/Models/AccountViewModels.cs
@@ -65,12 +65,16 @@ namespace HRAgencySystem.Web.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
