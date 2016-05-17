8a034dee955e91d02a0c6355fc116dc81301499d
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-05-17 17:58:44 +0300

    run mappings through global.asax

--- HRAgencySystem/HRAgencySystem.Web/Global.asax.cs
+++ HRAgencySystem/HRAgencySystem.Web/Global.asax.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using HRAgencySystem.Common.Mappings;
 
 namespace HRAgencySystem.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +16,12 @@ namespace HRAgencySystem.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
+            var autoMapperConfig = new AutoMapperConfig(new List<Assembly> { Assembly.GetExecutingAssembly() });
+            autoMapperConfig.Execute();
         }
     }
 }
