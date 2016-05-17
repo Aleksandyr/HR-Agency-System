8953c139ad87ed999dbca334e3fc11d965b28374
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-05-17 23:58:12 +0300

    delete identityModel and add my user and my dbContext in web project

--- HRAgencySystem/HRAgencySystem.Models/User.cs
+++ HRAgencySystem/HRAgencySystem.Models/User.cs
@@ -35,16 +35,12 @@
             }
 
         }
-
-        public class ApplicationUser : IdentityUser
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
-            public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-            {
-                // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-                var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-                // Add custom user claims here
-                return userIdentity;
-            }
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
         }
     }
 }
