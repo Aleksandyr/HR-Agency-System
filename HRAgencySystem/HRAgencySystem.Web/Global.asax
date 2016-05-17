4733cada99ed4ba4f6b04ffe475087c51ab682f5
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-05-17 17:49:52 +0300

    add web project skeleton

---
+++ HRAgencySystem/HRAgencySystem.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HRAgencySystem.Web.MvcApplication" Language="C#" %>
---
+++ HRAgencySystem/HRAgencySystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HRAgencySystem.Web.Startup))]
+namespace HRAgencySystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
