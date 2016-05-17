90788ece38c78c2bdd52148b3e71c0add882de7b
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-05-17 23:07:42 +0300

    add user model

---
+++ HRAgencySystem/HRAgencySystem.Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace HRAgencySystem.Models
+{
+    public class User
+    {
+        [Required]
+        public int RoleId { get; set; }
+
+        public virtual Role Role { get; set; }
+
+        public class ApplicationUser : IdentityUser
+        {
+            public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+            {
+                // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+                var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+                // Add custom user claims here
+                return userIdentity;
+            }
+        }
+    }
+}
