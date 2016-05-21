c4c1d3b520d5f592b9aa63796ff5c01d9eb016f6
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-05-21 23:03:58 +0300

    add search reservations by date

--- HRAgencySystem/HRAgencySystem.Web/Controllers/ReservationController.cs
+++ HRAgencySystem/HRAgencySystem.Web/Controllers/ReservationController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using HRAgencySystem.Common;
 using HRAgencySystem.Data.DataLayer;
 using HRAgencySystem.Web.ViewModels.Reservation;
+using HRAgencySystem.Web.ViewModels.User;
 using PagedList;
 
 namespace HRAgencySystem.Web.Controllers
@@ -21,13 +24,86 @@ namespace HRAgencySystem.Web.Controllers
         // GET: Reservation
         public ActionResult AllReservations(int? page)
         {
-            var getAllReservations = this.Data.Reservations
-                .All()
-                .OrderBy(r => r.StartDate)
-                .ProjectTo<ReservationViewModel>()
-                .ToPagedList(page ?? GlobalConstants.DefaultReservationsStartPage, GlobalConstants.DefaulReservationstPageSize);
+            string startDate = this.Request.QueryString["startDate"];
+            string endDate = this.Request.QueryString["endDate"];
+            var getAllReservations = new PagedList<ReservationViewModel>(null, page ?? GlobalConstants.DefaultReservationsStartPage,
+                        GlobalConstants.DefaulReservationstPageSize);
+
+            DateTime startDateToDateTime = new DateTime();
+            DateTime endDateToDateTime = new DateTime();
+
+            if (!string.IsNullOrEmpty(startDate) && !string.IsNullOrEmpty(endDate))
+            {
+                startDateToDateTime = DateTime.Parse(startDate);
+                endDateToDateTime = DateTime.Parse(endDate);
+
+                if (startDateToDateTime < endDateToDateTime)
+                {
+
+                    getAllReservations = (PagedList<ReservationViewModel>) this.Data.Reservations
+                        .All()
+                        .Where(r => r.StartDate >= startDateToDateTime && r.EndDate <= endDateToDateTime)
+                        .OrderBy(r => r.StartDate)
+                        .ProjectTo<ReservationViewModel>()
+                        .ToPagedList(page ?? GlobalConstants.DefaultReservationsStartPage,
+                            GlobalConstants.DefaulReservationstPageSize);
+                }
+            }
+            else if (!string.IsNullOrEmpty(startDate))
+            {
+                startDateToDateTime = DateTime.Parse(startDate);
+
+                getAllReservations = (PagedList<ReservationViewModel>)this.Data.Reservations
+                        .All()
+                        .Where(r => r.StartDate >= startDateToDateTime)
+                        .OrderBy(r => r.StartDate)
+                        .ProjectTo<ReservationViewModel>()
+                        .ToPagedList(page ?? GlobalConstants.DefaultReservationsStartPage,
+                            GlobalConstants.DefaulReservationstPageSize);
+            }
+            else if (!string.IsNullOrEmpty(endDate))
+            {
+                endDateToDateTime = DateTime.Parse(endDate);
+
+                getAllReservations = (PagedList<ReservationViewModel>)this.Data.Reservations
+                        .All()
+                        .Where(r => r.EndDate <= endDateToDateTime)
+                        .OrderBy(r => r.StartDate)
+                        .ProjectTo<ReservationViewModel>()
+                        .ToPagedList(page ?? GlobalConstants.DefaultReservationsStartPage,
+                            GlobalConstants.DefaulReservationstPageSize);
+            }
+            else
+            {
+                getAllReservations = (PagedList<ReservationViewModel>) this.Data.Reservations
+                    .All()
+                    .OrderBy(r => r.StartDate)
+                    .ProjectTo<ReservationViewModel>()
+                    .ToPagedList(page ?? GlobalConstants.DefaultReservationsStartPage,
+                        GlobalConstants.DefaulReservationstPageSize);
+            }
 
             return View(getAllReservations);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var reservation = this.Data.Reservations
+                .All()
+                .Include(r => r.Users)
+                .FirstOrDefault(r => r.Id == id);
+
+            var reservationDetailsViewModel = new ReservationDetailsViewModel()
+            {
+                Description = reservation.Description,
+                HallName = this.Data.Halls.All().Where(h => h.Id == reservation.HallId).Select(r => r.Name).FirstOrDefault(),
+                CapacityForReservation = reservation.CapacityForReservation,
+                StartDate = reservation.StartDate,
+                EndDate = reservation.EndDate,
+                Users = (IEnumerable<UserViewModel>) reservation.Users
+            };
+
+            return View(reservationDetailsViewModel);
         }
     }
 }
---
+++ HRAgencySystem/HRAgencySystem.Web/ViewModels/Reservation/ReservationDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace HRAgencySystem.Web.ViewModels.Reservation
+{
+    using System;
+    using System.Collections.Generic;
+
+    using HRAgencySystem.Common.Mappings;
+    using HRAgencySystem.Web.ViewModels.User;
+    using HRAgencySystem.Models;
+
+    public class ReservationDetailsViewModel : IMapFrom<Reservation>
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string HallName { get; set; }
+
+        public int CapacityForReservation { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public IEnumerable<UserViewModel> Users { get; set; }
+    }
+}
